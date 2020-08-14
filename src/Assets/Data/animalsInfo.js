8514c240cf13f8c57725bac967b1f1c3f9fcf171
Andrei Medvedev <andreiok@gmail.com>
2020-08-14 21:44:13 +0400

    feat: completed to collect animals info )))

--- src/Assets/Data/animalsInfo.js
+++ src/Assets/Data/animalsInfo.js
@@ -152,151 +152,151 @@ const animalsInfo = [
 	[
 		{
 			id: 1,
-			name: 'Жаворонок',
-			species: 'Alauda arvensis',
-			description: 'Жаворонки — перелетные птицы. С начала сентября они отлетают на юг. Возвращаются они в начале марта, независимо от того, сошел снег или нет. По прилету жаворонков определяли наступление весны и пору, когда пора пахать землю.',
-			image: 'https://live.staticflickr.com/65535/47105096764_f751fba568.jpg',
-			audio: 'https://www.xeno-canto.org/sounds/uploaded/ZNCDXTUOFL/XC462158-Skowronek_Alauda_arvensis_Poland_Jarek_Matusiak_%20-006%20skowronek%20%282%29.mp3'
+			name: 'Мартышки',
+			species: 'Cercopithecus',
+			description: 'Мартышки – это небольшие животные, хорошо развитые физически, достаточно подвижные и весьма интересные. Особый интерес представляет их сообразительность, а также невероятное любопытство.',
+			image: 'https://live.staticflickr.com/4114/4880593166_725a0e105c_c.jpg',
+			audio: 'https://zvukipro.com/uploads/files/2018-11/1541411058_monkey-chatter-2_z17aqavo1.mp3'
 		},
 		{
 			id: 2,
-			name: 'Соловей',
-			species: 'Luscinia luscinia',
-			description: 'Соловьи поют с начала мая и до конца лета. Каждая песня соловья состоит из 12 повторяющихся элементов, которые еще называют коленами. Кроме собственных трелей, соловьи легко и хорошо перенимают пение других птиц.',
-			image: 'https://live.staticflickr.com/7605/27669397735_f3c21758f2.jpg',
-			audio: 'https://www.xeno-canto.org/sounds/uploaded/HILVWSADVL/XC513809-R07_0052%20Thrush%20Nightingale%20Snipe.mp3'
+			name: 'Горилла',
+			species: 'Gorilla',
+			description: 'Рост взрослых самцов колеблется от 1,65 до 1,75 м. Встречались особи ростом 2 м; считается, что более высокого роста эти приматы не достигают. Однако известный советский зоолог Игорь Акимушкин утверждал, что рост самого крупного самца горной гориллы, убитого охотниками в начале XX века, составлял 2,32 м.',
+			image: 'https://live.staticflickr.com/1789/42716116604_96024587ad_k.jpg',
+			audio: 'https://zvukipro.com/uploads/files/2019-07/1562832382_eb87f0f579c3ba4.mp3'
 		},
 		{
 			id: 3,
-			name: 'Скворец',
-			species: 'Sturnus vulgaris',
-			description: 'Скворцы - перелётные птицы. Синхронный перелет больших стай скворцов называется мурмурацией. Это красивое и завораживающее явление – множество птиц будто танцуют в воздухе, образуя замысловатые фигуры, которые уменьшаются и увеличиваются в небе.',
-			image: 'https://live.staticflickr.com/65535/49357593971_9509fe1d7c.jpg',
-			audio: 'https://www.xeno-canto.org/sounds/uploaded/GYAUIPUVNM/XC515519-2020.01.01_17.24_01.MP3'
+			name: 'Орангутан',
+			species: 'Pongo',
+			description: 'Орангутаны — крупнейшие древесные обезьяны. Рост взрослых самцов может достигать 1,5 м, масса тела — 50—90 и даже 100 кг. Самки значительно меньше: около 1 м ростом при весе в 30—50 кг.',
+			image: 'https://live.staticflickr.com/5706/21443252268_5f404271bb_k.jpg',
+			audio: 'https://zvukipro.com/uploads/files/2019-07/1562832780_odi2nji5mta4mjy3mje_quho6nz1eqs.mp3'
 		},
 		{
 			id: 4,
-			name: 'Иволга',
-			species: 'Oriolus oriolus',
-			description: 'Мелодичность голоса иволги сравнивают со звучанием флейты. Человеку сложно разглядеть иволгу, так как она обитает высоко на деревьях. Иволга не только очень красивая, но и  полезная птица. Она уничтожает ядовитых гусениц, которых не поедают другие птицы.',
-			image: 'https://live.staticflickr.com/65535/47102184004_58a93380b9.jpg',
-			audio: 'https://www.xeno-canto.org/sounds/uploaded/GYAUIPUVNM/XC491801-2019.07.07_06.28_01.mp3'
+			name: 'Макака',
+			species: 'Macaca',
+			description: 'акаки — приматы средней величины с крепким туловищем и сильными конечностями. Их густая шерсть окрашена в серо-коричневый цвет, однако порою бывает и чёрной. На продолговатой морде волосяной покров отсутствует.',
+			image: 'https://live.staticflickr.com/65535/48263706731_76fab324a8_5k.jpg',
+			audio: 'https://zvukipro.com/uploads/files/2020-01/1578069402_596ee2effed961d.mp3'
 		},
 		{
 			id: 5,
-			name: 'Свиристель',
-			species: 'Bombycilla garrulus',
-			description: 'У свиристели очень цепкие коготки, что помогает птице удерживаться на ветках и склевывать ягоды, которые труднее всего достать. В период ухаживаний самец предлагает самке ягоду или другое угощение. Если самка его принимает, то птицы создают пару.',
-			image: 'https://live.staticflickr.com//65535//49367433842_1b06da0e6b.jpg',
-			audio: 'https://www.xeno-canto.org/sounds/uploaded/ZNCDXTUOFL/XC517421-AB-004%20%282%29%20jemio%C5%82uszka.mp3'
+			name: 'Гиббоны',
+			species: 'Hylobates',
+			description: 'Гиббоны (лат. Hylobates) — род человекообразных приматов, обитающих в Юго-Восточной Азии. Один из четырёх родов семейства гиббоновые. Раньше был единственным родом в этом семействе, но недавно в категорию родов были также выделены номаскусы, сиаманги и хулоки.',
+			image: 'https://live.staticflickr.com/7445/9484670538_2733972a9d_z.jpg',
+			audio: 'https://zvukipro.com/uploads/files/2020-02/1582036405_692202212af40b1.mp3'
 		},
 		{
 			id: 6,
-			name: 'Щегол',
-			species: 'Carduelis carduelis',
-			description: 'Щеглы поют красиво и мелодично. И в природе, и в неволе они щебечут почти круглый год. В пении щегла различают более 20 переливчатых трелей. Щеглы привыкают к людям, и даже могут возвратиться к хозяину после того, как их выпустили на волю',
-			image: 'https://live.staticflickr.com//65535//49366257253_db3ce48b9a.jpg',
-			audio: 'https://www.xeno-canto.org/sounds/uploaded/VOLIQOYWKG/XC489265-190724_07.58h_putter_biesbosch_%20boompjes%20langs%20open%20water_zang_1ex_ad_niet%20gezien_.mp3'
+			name: 'Гамадрил',
+			species: 'Papio hamadryas',
+			description: 'Большая обезьяна, достигающая одного метра длины (старые самцы), причём на хвост приходится 20—25 см; самки вдвое меньше. Общий цвет волос, покрывающих тело гамадрила, серый (цвета сухой травы); у самцов длинные, оригинально расположенные волосы на голове, плечах и груди образуют нечто вроде гривы',
+			image: 'https://live.staticflickr.com/8523/29207148875_58cf906c04_b.jpg',
+			audio: 'https://zvuki-tut.ru/samples/Gamadril.mp3'
 		}
 	],
 	[
 		{
 			id: 1,
-			name: 'Орёл',
-			species: 'Aquila nipalensis',
-			description: 'В древние времена орел был символом солнца. Орлы часто парят над землей, при этом скорость их перемещения может достигать 240 км/ч. Иллюзия медленного движения происходит из-за высоты полета – более 700 метров',
-			image: 'https://live.staticflickr.com//4835//43867392960_7105d71e19.jpg',
-			audio: 'https://www.xeno-canto.org/sounds/uploaded/KTBTZAHSXF/10_Aquila_nipalensis_al02D85.mp3'
+			name: 'Зайцы',
+			species: 'Lepus',
+			description: 'Зайцы живут поодиночке или парами. В отличие от кроликов, зайцы не роют нор, а сооружают гнёзда в небольших ямках. Зайчата рождаются развитыми, с шерстью и открытыми глазами, и мать остаётся с ними всего 5—6 дней, а затем лишь иногда прибегает к ним; вследствие этого много зайчат гибнет от врагов.',
+			image: 'https://live.staticflickr.com/4810/46713847241_018ef13ac1_3k.jpg',
+			audio: 'https://zvukipro.com/uploads/files/2018-11/1542655490_lyuboznayki_-z_uki-zhi_otnyh-krik-zayca.mp3'
 		},
 		{
 			id: 2,
-			name: 'Коршун',
-			species: 'Milvus migrans',
-			description: 'Коршуны – крупные хищники, в высоту они достигают около полуметра, а вес взрослых особей достигает 1 кг. Крылья узкие и длинные, их размах составляет 1,5 м. Коршуны часто гнездятся большими стаями и даже образуют крупные колонии.',
-			image: 'https://live.staticflickr.com//65535//48701190276_ee2a9ed594.jpg',
-			audio: 'https://www.xeno-canto.org/sounds/uploaded/SDPCHKOHRH/XC485740-2019-06-22%20Selenga%20Milan%20brun%20cris%20de%20quemandage%203.mp3'
+			name: 'Белки',
+			species: 'Sciurus',
+			description: 'Одной из широко известных отличительных особенностей многих белок является их способность запасать на зиму орехи. Некоторые виды белок закапывают их в землю, другие — прячут в дуплах деревьев.',
+			image: 'https://live.staticflickr.com/4109/5022023605_4b8bfff3f1_k.jpg',
+			audio: 'https://zvukipro.com/uploads/files/2020-04/1588141947_persian-squirrel-calls.mp3'
 		},
 		{
 			id: 3,
-			name: 'Лунь',
-			species: 'Circus cyaneus',
-			description: 'Лунь – это небольшой сокол. Питается в основном мышевидными грызунами, основа его рациона – полёвки, хомяки, мыши. Оперение луня может быть пепельно-серым. С такой птицей связано сравнение «седой, как лунь».',
-			image: 'https://live.staticflickr.com/65535/49322743903_32f3922d9e.jpg',
-			audio: 'https://www.xeno-canto.org/sounds/uploaded/BLMSIUFTFU/XC513498-190709_1175_Cir.cyan-f.mp3'
+			name: 'Песец',
+			species: 'Vulpes lagopus',
+			description: 'Единственный представитель семейства псовых, которому свойственен выраженный сезонный диморфизм окраски. По окраске различают обычного белого (зимой — чисто-белый, летом — грязно-бурый) и голубого песца.',
+			image: 'https://live.staticflickr.com/1763/42601535094_1f32930e67_b.jpg',
+			audio: 'https://zvuki-tut.ru/samples/Pesecc.mp3'
 		},
 		{
 			id: 4,
-			name: 'Сокол',
-			species: 'Falco peregrinus',
-			description: 'Латинское название сокола Falco произошло от слова «серп» из-за серповидной формы крыльев. Длинные и широкие крылья позволяют соколу высоко подниматься в небо и свободно парить. Скорость полёта сокола достигает 280-320 километров в час.',
-			image: 'https://live.staticflickr.com//65535//49310710607_92a3a145a9.jpg',
-			audio: 'https://www.xeno-canto.org/sounds/uploaded/MMEJYLOPDO/XC496049-Pilgrimsfalk_06.mp3'
+			name: 'Волк',
+			species: 'Canis lupus',
+			description: 'Разнообразие и диапазон частот голосовых средств волков значительно превосходят возможности абсолютного большинства животных (кроме человека и рукокрылых). Волки издают такие звуки, как вой, завывание, хныканье, ворчание, рычание, тявканье, визг, лай. Каждый звук имеет огромное количество вариаций.',
+			image: 'https://live.staticflickr.com/4773/40740512342_092255b544_k.jpg',
+			audio: 'https://zvukipro.com/uploads/files/2019-06/1559544955_audio_hero_wolf_digic05-01.mp3'
 		},
 		{
 			id: 5,
-			name: 'Ястреб',
-			species: 'Accipiter gentilis',
-			description: 'Для всех ястребов характерны широкие и короткие крылья. Ещё один отличительный признак - белые «брови» над глазами. Славянские дружинники размещали изображение ястреба на своих знаменах, как символ отваги, мощи и безжалостности к врагам.',
-			image: 'https://live.staticflickr.com//65535//49024617331_b9d0d2c9b3.jpg',
-			audio: 'https://www.xeno-canto.org/sounds/uploaded/MMEJYLOPDO/XC512740-Duvh%C3%B6k_09.mp3'
+			name: 'Лисица',
+			species: 'Vulpes vulpes',
+			description: 'Численность лисиц заметно колеблется по годам. На её состояние влияют такие факторы как количество грызунов, метеорологические условия, наличие в популяции инфекционных заболеваний.',
+			image: 'https://live.staticflickr.com/8478/27932564303_f3cec82d08_4k.jpg',
+			audio: 'https://zvukipro.com/uploads/files/2018-10/1539110277_zvuk-lisy-ururu.mp3'
 		},
 		{
 			id: 6,
-			name: 'Филин',
-			species: 'Bubo bubo',
-			description: 'Полет филина бесшумный, зрение очень острое. Эти особенности делают птицу непревзойденным ночным охотником. У филина нет естественных врагов, ни один зверь не охотится на взрослых птиц. А вот на птенцов нападают лисы и волки.',
-			image: 'https://live.staticflickr.com/65535/48137123012_393653c2e4.jpg',
-			audio: 'https://www.xeno-canto.org/sounds/uploaded/WNLIDKJKXT/XC518386-sense%20t%C3%ADtol.mp3'
+			name: 'Медведь',
+			species: 'Ursidae',
+			description: 'Обитают в самых разнообразных условиях — от степей до высокогорий, от лесов до арктических льдов, в связи с чем различаются образом жизни и способами питания. Большинство медведей обитает в равнинных или горных лесах умеренных и тропических широт, реже — на безлесных высокогорьях.',
+			image: 'https://live.staticflickr.com/65535/49212698543_f4d8007a99_4k.jpg',
+			audio: 'https://zvukipro.com/uploads/files/2018-10/1538581767_bear-bear.mp3'
 		}
 	],
 	[
 		{
 			id: 1,
-			name: 'Альбатрос',
-			species: 'Diomedea exulans',
-			description: 'Альбатрос - самая крупная летающая птица в мире. Размах крыльев достигает три с половиной, вес - десять килограммов. Большую часть жизни альбатросы проводят в воздухе, покрывая над океанскими просторами огромные расстояния',
-			image: 'https://live.staticflickr.com/7557/16260253965_8e9430cb66.jpg',
-			audio: 'https://www.xeno-canto.org/sounds/uploaded/WOEAFQRMUD/XC293087-Diomedea%20exulans151120_T254.mp3'
+			name: 'Жираф',
+			species: 'Giraffa camelopardalis',
+			description: 'Шея у жирафов необычайно длинная, и это несмотря на то, что у них, как и почти у всех других млекопитающих, лишь семь шейных позвонков. Высокий рост увеличивает нагрузку на систему кровообращения, прежде всего в отношении кровоснабжения головного мозга. Поэтому сердце у жирафов особенно сильное.',
+			image: 'https://live.staticflickr.com/4055/4444848296_f16d6c4195_4k.jpg',
+			audio: 'https://zvukipro.com/uploads/files/2019-06/1561704378_kak-govorit-zhiraf.mp3'
 		},
 		{
 			id: 2,
-			name: 'Олуша',
-			species: 'Sula nebouxii',
-			description: 'Особенностью голубоногой олуши является не только насыщенный ярко-синий цвет лапок, но еще и тот факт, что они очень теплые. В то время как другие виды птиц греют кладки своим телом, голубоногая олуша делает это с помощью лапок',
-			image: 'https://live.staticflickr.com/800/40645471394_4422e69ed8.jpg',
-			audio: 'https://www.xeno-canto.org/sounds/uploaded/YHKQPPJDVP/XC411507-171217_1491%20BF%20Booby%205ft%20IDLP%201230%20mp3%20amp.mp3'
+			name: 'Слон',
+			species: 'Elephantidae',
+			description: 'Слоны — самые крупные наземные животные на Земле. Обитают они в Юго-Восточной Азии и Африке в тропических лесах и саваннах.',
+			image: 'https://live.staticflickr.com/7368/12978345353_574c730718_h.jpg',
+			audio: 'https://zvukipro.com/uploads/files/2018-10/1540913446_z_uk-slona.mp3'
 		},
 		{
 			id: 3,
-			name: 'Буревестник',
-			species: 'Puffinus griseus',
-			description: 'Размеры буревестниковых разные. Самые маленькие из них в длину составляют до 25 см, самые большие - до 1 м, при размахе крыльев около 2 м. Существует поверье, что появление буревестника в воздухе предвещает бурю, о чем говорит само название птицы.',
-			image: 'https://live.staticflickr.com//607//22136056020_935cb113f9.jpg',
-			audio: 'https://www.xeno-canto.org/sounds/uploaded/XQEVNREHJY/SHEARWATER%20Christmas%20Island_04_Motu_Isla%20de%20Pascua-Easter%20Island_CH_4MAR03_Alvaro%20Jaramillo.mp3'
+			name: 'Буйвол',
+			species: 'Syncerus caffer',
+			description: 'Рога африканского буйвола очень своеобразны. Характерной их особенностью является то, что у взрослых быков основания рогов на лбу срастаются, образуя нечто вроде сплошного костного щита, который не всегда может пробить даже винтовочная пуля.',
+			image: 'https://live.staticflickr.com/5130/5343539251_ba918da8ff_c.jpg',
+			audio: 'https://zvukipro.com/uploads/files/2019-12/1577123335_6266e1214ab5f06.mp3'
 		},
 		{
 			id: 4,
-			name: 'Пеликан',
-			species: 'Pelecanus',
-			description: 'Пеликаны — обитатели морей и рек. Ходят они неуклюже, но хорошо летают и плавают. Питаются в основном рыбой, устраивают коллективные охоты — выстроившись полукругом хлопают по воде крыльями и клювами и вытесняют напуганную рыбу на мелководье.',
-			image: 'https://live.staticflickr.com/7885/46523771945_9496c2a191.jpg',
-			audio: 'https://www.xeno-canto.org/sounds/uploaded/XAMHIHFTZG/XC331138-call1.mp3'
+			name: 'Бегемот',
+			species: 'Hippopotamus amphibius',
+			description: 'Характерной особенностью бегемота является его полуводный образ жизни — бо́льшую часть времени он проводит в воде, выходя на сушу лишь ночью на несколько часов для кормёжки. Бегемот обитает только у пресных водоёмов, хотя может изредка оказываться в море.',
+			image: 'https://live.staticflickr.com/7298/12029886846_5884bd3166_k.jpg',
+			audio: 'https://zvukipro.com/uploads/files/2018-12/1545214257_begemot-z_uk.mp3'
 		},
 		{
 			id: 5,
-			name: 'Пингвин',
-			species: 'Aptenodytes forsteri',
-			description: 'Самец императорского пингвина достигает роста 130 см, его масса может приближаться к 50 кг. Из всех современных пингвинов этот вид – самый крупный. Питание пингвина состоит из рыбы, кальмаров и криля. Охотятся птицы в океане большими группами.',
-			image: 'https://live.staticflickr.com/5202/5252413926_8e013a3efd.jpg',
-			audio: 'https://www.xeno-canto.org/sounds/uploaded/OOECIWCSWV/XC449827-LS100829%20King%20Penguin%20call%20A.mp3'
+			name: 'Ленивец',
+			species: 'Folivora Delsuc',
+			description: 'Ленивец величиной со среднюю собаку. У него длинная жесткая шерсть, имеющая зеленоватый окрас. Такой оттенок шубе зверя придают сине-зелёные водоросли, которые поселяются в его влажной шерсти.',
+			image: 'https://live.staticflickr.com/7761/18243711431_15a4aec84d_c.jpg',
+			audio: 'https://zvuki-tut.ru/samples/Lenivecc.mp3'
 		},
 		{
 			id: 6,
-			name: 'Чайка',
-			species: 'Larus argentatus',
-			description: 'Чайки населяют берега морей, озёр, рек, водохранилищ, болот, часто гнездятся на островах. С конца прошлого века чайки стали появляться в крупных городах, где устраивает гнёзда на крышах зданий. Все чайки ведут колониальный образ жизни.',
-			image: 'https://live.staticflickr.com/65535/48577115317_7034201dde.jpg',
-			audio: 'https://www.xeno-canto.org/sounds/uploaded/VOLIQOYWKG/XC501190-190801_06.50h_zilvermeeuw_duinen%20van%20goeree_roep_2ex_overvliegend_gezien_.mp3'
+			name: 'Верблюд',
+			species: 'Camelus',
+			description: 'Эти млекопитающие хорошо приспособлены к жизни в суровой и безводной местности. У верблюдов имеются грудная, запястные, локтевые и наколенные мозоли.',
+			image: 'https://live.staticflickr.com/64/197743545_eb1f9cbbc5_b.jpg',
+			audio: 'https://zvuki-tut.ru/samples/Lenivecc.mp3'
 		}
 	]
 ];
